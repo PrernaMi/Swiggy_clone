d46430a529e2e5cb3ddb63ef9b7ebb606a66f88e
Prerna <prernamittal440@gmail.com>
2024-09-22 17:36:39 +0530

    Initial Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Google maps"),
+      ),
+      body: Container(),
+    );
+  }
+}
